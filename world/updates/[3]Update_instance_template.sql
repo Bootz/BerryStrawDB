9ffdff2430b67dc146d3fc24307642dc97f7ebf0
Alexandros Papadopoulos <alexfred@live.com>
2012-04-24 00:38:31 +0300

    [4] Another Mistake, FOOLISH ME. Fixed. Atlast!

--- world/updates/[3]Update_instance_template.sql
+++ world/updates/[3]Update_instance_template.sql
@@ -1,2 +1,3 @@
-UPDATE `instance_template` SET `map`=940 WHERE  `map`=951 LIMIT 1;
+UPDATE `instance_template` SET `map`=941 WHERE  `map`=951 LIMIT 1;
 UPDATE `instance_template` SET `map`=951 WHERE  `map`=940 LIMIT 1;
+UPDATE `instance_template` SET `map`=940 WHERE  `map`=941 LIMIT 1;
