13ccc846f4c123c26189350f4a449052c5684e5f
FailZorD <alexfred@live.com>
2012-04-24 21:22:04 +0300

    Removing the old Updates.
    Signed-off-by: FailZorD <alexfred@live.com>

--- world/updates/[3]Update_instance_template.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `instance_template` SET `map`=941 WHERE  `map`=951 LIMIT 1;
-UPDATE `instance_template` SET `map`=951 WHERE  `map`=940 LIMIT 1;
-UPDATE `instance_template` SET `map`=940 WHERE  `map`=941 LIMIT 1;
