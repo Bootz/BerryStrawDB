dafdbb994b71e67f8973267e4ea30eba961db7ce
Alex Papadopoulos <alexfred@live.com>
2012-05-28 15:08:00 +0300

    Added Updates These are the new updates for the Strawberry DB. Taken and based from the Core. Enjoy! 28/05/2012
    Signed-off-by: Alex Papadopoulos <alexfred@live.com>

---
+++ world/scripts/[1]Script [Spell Proc Event].sql
@@ -0,0 +1,4 @@
+﻿-- (41635) Prayer of Mending
+DELETE FROM `spell_proc_event` WHERE `entry` IN (41635);
+INSERT INTO `spell_proc_event` VALUES (41635, 0x02, 0x06, 0x00000000, 0x00000000, 0x00000000, 0x000AA2A8, 0x00000000, 0, 100, 0);
+
