1a2915bb4b0858fceebcf9caf76ee5ca4140a740
iFailZorD <alexfred@live.com>
2012-06-06 12:00:10 +0300

    New Updates

--- world/updates/[3]Error_Fixes.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `creature_model_info` SET `modelid_other_gender`=6895 WHERE  `modelid`=6894 LIMIT 1;
-UPDATE `creature_model_info` SET `modelid_other_gender`=6894 WHERE  `modelid`=6895 LIMIT 1;
-UPDATE `creature_model_info` SET `modelid_other_gender`=29423 WHERE  `modelid`=29422 LIMIT 1;
-UPDATE `creature_model_info` SET `modelid_other_gender`=29422 WHERE  `modelid`=29423 LIMIT 1;
--- world/updates/[5]Structure [Quest_Template].sql
+++
@@ -1,3 +0,0 @@
--- Increase field size.
-
-ALTER TABLE `quest_template` CHANGE `RequiredRaces` `RequiredRaces` INT(7) UNSIGNED DEFAULT 0 NOT NULL;
